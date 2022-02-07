04acf5e7812b6373aeb5a40bc9de8e7b7a584f86
Paulo Roberto <paulo.roberto@passeidireto.int>
2022-02-06 21:47:16 -0300

    :yellow_heart: comando no cli para envio de twitters para função lambda;

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 import boto3
 import click
 
@@ -16,7 +17,16 @@ config = {
 }
 
 
-@click.command()
+def __sqs_send_message(sqs, queue_url, tw, text):
+    return sqs_send_message(sqs, queue_url, tweet_in_message_attributes_sqs(tw), text)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command('tw-to-sqs')
 @click.option('--queue-url', default=config.get('SQS_QUEUE_URL'), help='SQS queue url.')
 @click.option('--twitter-token', default=config.get('TWITTER_BEARER_TOKEN'), help='Bearer token Twitter.')
 @click.option('--one-twitter', default=False, help='Send only one Tweet.')
@@ -36,5 +46,20 @@ def cli_sqstw(queue_url, twitter_token, one_twitter):
         print(message_id)
 
 
+@cli.command('tw-to-lambda')
+@click.option('--url', default=config.get('API_GATEWAY_URL'), help='Api Gateway URL.')
+@click.option('--twitter-token', default=config.get('TWITTER_BEARER_TOKEN'), help='Bearer token Twitter.')
+@click.option('--one-twitter/--all-twitters', default=False, help='Send only one Tweet.')
+def cli_tw_to_lambda(url, twitter_token, one_twitter):
+    tw_data = search_tweets(twitter_token).get('data')
+    if not one_twitter:
+        for tweet in tw_data:
+            req = requests.post(url, json=tweet, headers={'Content-Type': 'application/json'})
+            print(req.json())
+    else:
+        req = requests.post(url, json=tw_data[0])
+        print(req.json())
+
+
 if __name__ == '__main__':
-    cli_sqstw()
+    cli()
